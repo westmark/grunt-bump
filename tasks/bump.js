3f75410c9f395fe4176a76ff1272ef8f227f34c0
Fredrik Westmark <fredrik.westmark@gmail.com>
2013-02-01 12:51:25 +0100

    Add Gruntfile.js. Added version type "build".
    If the version type is set to "build", the version itself won't be
    bumped. The build number will always be bumped no matter what happens
    to the version.

--- tasks/bump.js
+++ tasks/bump.js
@@ -15,34 +15,60 @@ module.exports = function(grunt) {
     var PACKAGE_FILE = 'package.json';
     var _package = grunt.file.readJSON(PACKAGE_FILE);
     var file = grunt.file.read( PACKAGE_FILE );
+    var buildRe = /([\'|\"]build[\'|\"]:\s*[\'|\"])(\d*)([\'|\"])/i,
+        versionRe = /((\s*)[\'|\"]version[\'|\"]:[ ]*[\'|\"])([\d|.]*)([\'|\"])/i;
 
     // compute the new version
-    var version = bump(_package.version, versionType || 'patch');
+    var version = bumpVersion(_package.version, versionType || 'patch'),
+        build = bumpBuild(_package.build),
+        str = file.match(versionRe)[0];
 
-    file = file.replace( /([\'|\"]version[\'|\"]:[ ]*[\'|\"])([\d|.]*)([\'|\"])/i, function( match, left, center, right ) {
-      return left + version + right;
-    } );
+    file = file.replace(versionRe, function( match, left, indent, center, right ) {
+      var str = left + version + right;
+      if (!file.match(buildRe)) {
+        str += "," + indent + '"build": "0"';
+      }
+      return str;
+    });
+
+    file = file.replace(buildRe, function( match, left, center, right ) {
+      return left + build + right;
+    });
 
     grunt.file.write( PACKAGE_FILE, file );
 
-    grunt.log.ok('Version bumped to ' + version);
+    grunt.log.ok('At version ' + version + ':' + build);
   });
 
 
-  function bump (version, versionType) {
+  function bumpVersion (version, versionType) {
     var type = {
       patch: 2,
       minor: 1,
-      major: 0
+      major: 0,
+      build: false
     };
 
     var parts = version.split('.');
     var idx = type[versionType || 'patch'];
 
-    parts[idx] = parseInt(parts[idx], 10) + 1;
-    while(++idx < parts.length) {
-      parts[idx] = 0;
+    if (idx !== false) {
+      parts[idx] = parseInt(parts[idx], 10) + 1;
+      while(++idx < parts.length) {
+        parts[idx] = 0;
+      }
     }
     return parts.join('.');
+  }
+
+  function bumpBuild (build) {
+    if (build) {
+      build = parseInt(build, 10) + 1;
+    }
+    else {
+      build = 1;
+    }
+
+    return build;
   }
 };
